796221362b60aeb76a1da258bfdaa1fd3efee698
LucasNiello <terminiello.lucas@gmail.com>
2026-05-29 14:36:42 -0300

    feat: redirect autenticado de / para /admin

--- SenaiGMP/routes/web.php
+++ SenaiGMP/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
+Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect('/admin');
+    }
+    return view('welcome');
+})->name('home');
 
 Route::view('/institucional', 'welcome')->name('institutional');
