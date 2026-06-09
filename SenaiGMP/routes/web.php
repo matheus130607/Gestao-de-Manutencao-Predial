eddcb2e256f651210cca71635b30308c19856daf
jefferson-kkk <jecorrea.2008@gmail.com>
2026-06-09 15:51:02 -0300

    atualização 09.06.2026

--- SenaiGMP/routes/web.php
+++ SenaiGMP/routes/web.php
@@ -11,7 +11,11 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/procedimentos-operacionais', function () {
-    return response()->file(base_path('../Documentacao/procedimentos-operacionais.html'));
+    $path = base_path('../home/procedimentos-operacionais.html');
+    if (!file_exists($path)) {
+        abort(404);
+    }
+    return response()->file($path);
 })->name('procedimentos.operacionais');
 
 Route::view('/institucional', 'welcome')->name('institutional');
